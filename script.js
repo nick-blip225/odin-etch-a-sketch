204a73463d8283f9d8c01e013ccc8de9117926c5
nick-blip225 <224284647+nick-blip225@users.noreply.github.com>
2026-01-07 22:37:37 -0600

    Create 16x16 sketchpad
    Created 16x16 grid with js and styled with css.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-script
+body = document.querySelector("body");
+sketchPad = document.createElement("div");
+sketchPad.setAttribute("id", "sketchPad");
+
+document.body.appendChild(sketchPad);
+//sketchPad.textContent = "Hello World"
+
+for (let index = 0; index < 16; index++) {
+    rowDiv = document.createElement("div");
+    rowDiv.classList.add("rowDiv");
+    sketchPad.appendChild(rowDiv);
+    //rowDiv.textContent = "test"
+    for (let index = 0; index < 16; index++) {
+        columnDiv = document.createElement("div");
+        columnDiv.classList.add("columnDiv");
+        rowDiv.appendChild(columnDiv);
+        //columnDiv.textContent = "More tests";
+    }
+}
